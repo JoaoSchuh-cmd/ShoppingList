3612fe5d7673611b7c3966f05e625c90d128a402
João Schuh <82126946+JoaoSchuh-cmd@users.noreply.github.com>
2024-11-12 21:53:58 -0300

    2# Configure Hilt

---
+++ app/src/main/java/br/com/pucpr/shoppinglist/ShoppingListApplication.kt
@@ -0,0 +1,9 @@
+package br.com.pucpr.shoppinglist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShoppingListApplication: Application() {
+
+}
